8c797d4f582d81b99d453a01f7a6e0b176336820
David Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-04-05 18:23:20 +0200

    Feature/WithBoundedLogger mixin improved
    * Added additional extra fields from request.
    * Cleanup of none extra values.

--- connect/processors_toolkit/logger/mixins.py
+++ connect/processors_toolkit/logger/mixins.py
@@ -10,21 +10,37 @@ class WithBoundedLogger:
 
     def bind_logger(self, request: Union[RequestBuilder, dict]) -> LoggerAdapter:
         """
-        Binds the logger to the given request by attaching the id to
-        the extra data of the logger adapter.
+        Binds the logger to the given request by attaching the id and some
+        additional information to the extra data of the logger adapter.
 
         :param request: Union[RequestBuilder, dict] The request to extract the ids
         :return: LoggerAdapter
         """
-        from_request = {}
-        if isinstance(request, RequestBuilder) and request.id() is not None:
-            from_request.update({'id': request.id()})
+        if not isinstance(request, RequestBuilder):
+            if not isinstance(request, dict):
+                return self.logger
+            request = RequestBuilder(request)
 
-        elif isinstance(request, dict) and request.get('id') is not None:
-            from_request.update({'id': request.get('id')})
+        from_request = {
+            'request_id': request.id(),
+            'request_type': request.type(),
+            'request_status': request.status(),
+        }
+
+        if request.is_asset_request():
+            from_request['tier_id'] = request.asset().asset_tier_customer('id')
+            from_request['asset_id'] = request.asset().asset_id()
+
+        elif request.is_tier_config_request():
+            from_request['tier_id'] = request.tier_configuration().tier_configuration_account('id')
+            from_request['tier_config_id'] = request.tier_configuration().tier_configuration_id()
 
         self.logger = ExtensionLoggerAdapter(
             self.logger.logger,
-            {**self.logger.extra, **from_request},
+            {
+                **self.logger.extra,
+                **{k: v for k, v in from_request.items() if v is not None},
+            },
         )
+
         return self.logger
