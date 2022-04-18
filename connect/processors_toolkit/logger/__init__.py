4d740e34957e2658a18c5a6b572d218b28ced583
David Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-04-18 16:24:37 +0200

    Feature/Added disclaimer.

--- connect/processors_toolkit/logger/__init__.py
+++ connect/processors_toolkit/logger/__init__.py
@@ -1,3 +1,8 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect Processors Toolkit.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
 from .adapter import (  # noqa: F401
     bind_logger,
     ExtensionLoggerAdapter,
