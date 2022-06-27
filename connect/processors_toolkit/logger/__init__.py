e55632eb966bbb0dbec61f0ccb3052e80752de24
Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-06-27 15:23:28 +0200

    Feature/Decoupled logger extension functionality.

--- connect/processors_toolkit/logger/__init__.py
+++ connect/processors_toolkit/logger/__init__.py
@@ -1,13 +1,4 @@
-from logging import LoggerAdapter
-
-
-class ExtensionLoggerAdapter(LoggerAdapter):
-    def process(self, msg, kwargs):
-        extra = kwargs.get("extra", {})
-        extra.update(self.extra)
-        kwargs['extra'] = extra
-
-        if 'request_id' in extra:
-            msg = f"{extra.get('request_id')} {msg}"
-
-        return msg, kwargs
+from .adapter import (  # noqa: F401
+    bind_logger,
+    ExtensionLoggerAdapter,
+)
