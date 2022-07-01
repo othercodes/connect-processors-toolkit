226a4ceef143b9bd46ea52b8b47d74d2ec0c4e9d
Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-07-01 11:53:44 +0200

    Feature/Add disclaimer. (#15)

--- connect/processors_toolkit/application/container.py
+++ connect/processors_toolkit/application/container.py
@@ -1,3 +1,8 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect Processors Toolkit.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
 from __future__ import annotations
 
 import pinject
--- connect/processors_toolkit/requests/exceptions.py
+++ connect/processors_toolkit/requests/exceptions.py
@@ -1,3 +1,8 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect Processors Toolkit.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
 from typing import Optional
 
 
